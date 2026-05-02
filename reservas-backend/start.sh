eb68e520155b9fe15a796a1af01c9d72243f740a
Emmanuel-EEB <emmanuel.esteban.eb@gmail.com>
2026-05-01 20:18:00 -0400

    Fix sqlite permissions

--- reservas-backend/start.sh
+++ reservas-backend/start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+chown -R www-data:www-data /var/www/database /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/database /var/www/storage /var/www/bootstrap/cache
+
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
